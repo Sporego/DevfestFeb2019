8c92485690973e219dbd2f5210172f4438cd7785
Sporego <Hello@Filbert.me>
2019-02-03 01:12:07 -0500

    survey added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+// houdini()
+// Makes Div Display and Hide Toggle
+
 function houdini() {
   var x = document.getElementById("effortFilter");
   if (x.style.display === "none") {
@@ -6,3 +9,71 @@ function houdini() {
     x.style.display = "none";
   }
 }
+
+//
+var surveyValueChanged = function(sender, options) {
+  var el = document.getElementById(options.name);
+  if (el) {
+    el.value = options.value;
+  }
+};
+
+var json = {
+  questions: [
+    {
+      type: "checkbox",
+      name: "car",
+      title: "What car are you driving?",
+      isRequired: true,
+      colCount: 4,
+      choices: [
+        "None",
+        "Ford",
+        "Vauxhall",
+        "Volkswagen",
+        "Nissan",
+        "Audi",
+        "Mercedes-Benz",
+        "BMW",
+        "Peugeot",
+        "Toyota",
+        "Citroen"
+      ]
+    },
+    {
+      type: "radiogroup",
+      name: "price",
+      title: "Do you feel our current price is merited by our product?",
+      choices: [
+        {
+          value: "correct",
+          text: "Yes, the price is about right"
+        },
+        {
+          value: "low",
+          text: "No, the price is too low for your product"
+        },
+        {
+          value: "high",
+          text: "No, the price is too high for your product"
+        }
+      ]
+    }
+  ]
+};
+
+window.survey = new Survey.Model(json);
+
+survey.onComplete.add(function(result) {
+  document.querySelector("#surveyResult").innerHTML =
+    "result: " + JSON.stringify(result.data);
+});
+
+survey.data = {
+  car: ["Ford"]
+};
+
+$("#surveyElement").Survey({
+  model: survey,
+  onValueChanged: surveyValueChanged
+});
