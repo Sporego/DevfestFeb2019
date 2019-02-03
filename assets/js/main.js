5427bf11db02fb62b4fb66e3874067ee4bb9acaf
Sarah Seidman <ss5311@barnard.edu>
2019-02-03 06:06:51 -0500

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,22 +10,6 @@ function houdini() {
   }
 } 
 
-function changeImage() {
-  var x = prompt("This is a family photo: What's the key fam?");
-  if (x == "correct") {
-    alert("yes!");
-    document.getElementById("imgClickAndChange").src =
-      "https://i.pinimg.com/originals/61/1b/10/611b102fa975e0b92ac762d241efb947.jpg";
-  }
-}
-function changeImage2() {
-  var x = prompt("This is a family photo: What's the key fam?");
-  if (x == "correct") {
-    alert("yes!");
-    document.getElementById("imgClickAndChange2").src =
-      "http://disneyexaminer.com/wp-content/uploads/2016/11/disney-holiday-season-shopping-black-friday-gift-ideas-2016-star-wars-family-sleepwear-collection-wookie-bb-8-pajamas.jpg";
-  }
-}
 //
 var surveyValueChanged = function(sender, options) {
   var el = document.getElementById(options.name);
@@ -34,55 +18,27 @@ var surveyValueChanged = function(sender, options) {
   }
 };
 
+
 var json = {
   questions: [
     {
-      type: "radiogroup",
-      name: "tickle",
-      title: "If I told you I was ticklish would you tickle me?",
-      choices: [{ value: "item1", text: "Yes" }, { value: "item2", text: "No" }]
-    },
+      type:"radiogroup",
+      name:"tickle",
+      title:"If I told you I was ticklish would you tickle me?",
+      choices:[{value:"item1",text:"Yes"},{value:"item2",text:"No"}]},
     {
-      type: "radiogroup",
-      name: "cook",
-      title: "You have friends coming over, what would you cook?",
-      choices: [
-        { value: "item1", text: "Something on the grill" },
-        { value: "item2", text: "Something in the oven" },
-        { value: "item3", text: "Something in the microwave" },
-        { value: "item4", text: "Something on the stove" }
-      ]
-    },
+      type:"radiogroup",
+      name:"cook",
+      title:"You have friends coming over, what would you cook?",
+      choices:[{value:"item1",text:"Something on the grill"},
+      {value:"item2",
+      text:"Something in the oven"},
+      {value:"item3",
+      text:"Something in the microwave"},
+      {value:"item4",
+      text:"Something on the stove"}
+    ]},
     {
-      type: "imagepicker",
-      name: "spirit",
-      title: "Spirit Animal",
-      choices: [
-        { value: "charizard", imageLink: "./assets/img/charizard.png" },
-        { value: "lion", imageLink: "./assets/img/lion.png" },
-        { value: "sea turtle", imageLink: "./assets/img/sea-turtle.jpg" },
-        { value: "tardigrade", imageLink: "./assets/img/tardigrade.jpg" }
-      ]
-    },
-
-    {
-      type: "radiogroup",
-      name: "quote",
-      title: "What quote is BS?",
-      choices: [
-        {
-          value: "correct",
-          text: '"I wanna live \'till I die, no more, no less."'
-        },
-        {
-          value: "low",
-          text:
-            '"If anyone slaps you on the right cheek turn your left cheek also"'
-        },
-        { value: "high", text: '"Fish meat is practically a vegetable."' },
-        { value: "item1", text: '"You can\'t always get what you want."' }
-      ]
-    }
       type:"imagepicker",
       name:"spirit",
       title:"Spirit Animal",
